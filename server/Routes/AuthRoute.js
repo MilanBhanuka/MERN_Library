409b3f16d9e97d4b449baf0fb17f612a97cee8d2
MilanBhanuka <vgmbhanuka@gmail.com>
2023-09-06 17:13:17 +0530

    middleware added

--- server/Routes/AuthRoute.js
+++ server/Routes/AuthRoute.js
@@ -1,7 +1,9 @@
 const { Signup, Login } = require("../Controllers/AuthController");
+const {userVerification} = require("../Middleware/AuthMiddleware");
 const router = require("express").Router();
 
 router.post("/signup", Signup);
 router.post("/login", Login);
+router.post("/", userVerification);
 
 module.exports = router;
